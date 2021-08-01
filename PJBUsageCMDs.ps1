2ba2c44c97a381ff5bb62a3433c26484ed543445
Patrick Burke <24pburke@gmail.com>
2021-08-01 17:05:36 -0500

    Testing out comments

--- PJBUsageCMDs.ps1
+++ PJBUsageCMDs.ps1
@@ -5,27 +5,27 @@ Import-Module OktaAPI
 .\OktaAPISettings.ps1
 
 #Show information about the current user.
-Get-OktaUser "me"
+<# Get-OktaUser "me" #>
 
 #Add user to a group.
-$user = Get-OktaUser "00uojcckkuFU9jzRs3l6" #literal in parentheses can be User ID or "me" for quick testing.
+<# $user = Get-OktaUser "00uojcckkuFU9jzRs3l6" #literal in parentheses can be User ID or "me" for quick testing.
 $groupR = Get-OktaGroups "Marketing" 'type eq "OKTA_GROUP"' #use name for simplicity, need to specify if group is Okta-mastered.
-Add-OktaGroupMember $groupR.id $user.id
+Add-OktaGroupMember $groupR.id $user.id #>
 
 # Create a user.
-$userprofile = @{login = "green@color.com"; email = "green@color.com"; firstName = "Green"; lastName = "Guy"}
+<# $userprofile = @{login = "green@color.com"; email = "green@color.com"; firstName = "Green"; lastName = "Guy"}
 $user = New-OktaUser @{profile = $userprofile}
-Write-Host "User Creation attempted. Admin needs to confirm."
+Write-Host "User Creation attempted. Admin needs to confirm." #>
 
 # Create a group.
-$groupprofile = @{name = "PowerShell"; description = "Created programmatically in PowerShell"}
+<# $groupprofile = @{name = "PowerShell"; description = "Created programmatically in PowerShell"}
 $groupC = New-OktaGroup @{profile = $groupprofile}
-Write-Host "Group Creation attempted. Admin needs to confirm."
+Write-Host "Group Creation attempted. Admin needs to confirm." #>
 
 # Get all users. If you have more than 200 users, you have to use pagination.
 # See this page for more info:
 # https://developer.okta.com/docs/reference/api-overview/#pagination
-$params = @{filter = 'status eq "ACTIVE"'}
+<# $params = @{filter = 'status eq "ACTIVE"'}
 do {
     $page = Get-OktaUsers @params
     $users = $page.objects
@@ -34,28 +34,40 @@ do {
         Write-Host $user.profile.login $user.profile.email
     }
     $params = @{url = $page.nextUrl}
-} while ($page.nextUrl)
+} while ($page.nextUrl) #>
 
 # Query up to 200 users by first name, last name or email.
-$pagequeried = Get-OktaUsers -q "Patrick"
+<# $pagequeried = Get-OktaUsers -q "Patrick"
 $usersqueried = $pagequeried.objects
 Write-Host $pagequeried
-Write-Host $usersqueried
+Write-Host $usersqueried #>
 
 # Filter lists all users; filter by status, last updated, id, login, email, first name or last name.
-$pagefiltered = Get-OktaUsers -filter 'profile.firstName eq "Patrick"'
+<# $pagefiltered = Get-OktaUsers -filter 'profile.firstName eq "Patrick"'
 $usersfiltered = $pagefiltered.objects # see pagination above.
 Write-Host $pagefiltered
-Write-Host $usersfiltered
+Write-Host $usersfiltered #>
 
 #Quick note: More optionality (attributes to be filtered for) with -filter vs. -q
 #More details: https://developer.okta.com/docs/reference/api/users/#request-parameters-3
 
 # Search lists all users; search by any user profile property, including custom-defined
 # properties, and id, status, created, activated, status changed and last updated.
-$pagesearched = Get-OktaUsers -search 'profile.department eq "Marketing"'
+<# $pagesearched = Get-OktaUsers -search 'profile.department eq "Marketing"'
 $userssearched = $pagesearched.objects # see pagination above.
 Write-Host $pagesearched
-Write-Host $userssearched
+Write-Host $userssearched #>
 
-#Dummycomment
+#Adding users to Okta org with a for loop
+<# for ($i=0; $i -lt 5; $i++)
+{
+    $date = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
+    $userprofile = @{login = "$date@getcolor.com"; email = "$date@getcolor.com"; firstName = "Green"; lastName = "Guy"}
+    $user = New-OktaUser @{profile = $userprofile}
+    Write-Host addedTBD
+} #>
+
+#Get-OktaUsers(<#$q, $filter, $limit = 200, $url = "/api/v1/users?q=$q&filter=$filter&limit=$limit&search=$search", $search#>)
+#Lists users from a certain username
+<# $usersListed = Get-OktaUsers ($q = "cm@the-sopranos.com")
+$usersListed.response.Content #>
